b23e9935ddbc87cca8863391a958200a97d7ee03
mcHiep <hiep.ikong@gmail.com>
2023-03-14 00:12:10 +0700

    update

--- app/src/main/java/com/neko/hiepdph/calculatorvault/activities/HomeActivity.kt
+++ app/src/main/java/com/neko/hiepdph/calculatorvault/activities/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.neko.hiepdph.calculatorvault.activities
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -48,37 +50,48 @@ class HomeActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         setupActionBarWithNavController(navController, binding.drawerLayout)
         binding.navigationView.setupWithNavController(navController)
-        val config = AppBarConfiguration.Builder(navController.graph).setOpenableLayout(
+        val config = AppBarConfiguration.Builder(
+            setOf(
+                R.id.fragmentLanguage,
+                R.id.fragmentVault,
+                R.id.fragmentSetting,
+                R.id.fragmentNote,
+                R.id.fragmentBrowser,
+                R.id.fragmentRecycleBin
+            )
+        ).setOpenableLayout(
             binding.drawerLayout
         ).build()
 
         NavigationUI.setupWithNavController(binding.toolbar, navController, config)
 
 
-        binding.navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.item_vault -> navController.navigate(R.id.fragmentVault)
-                R.id.item_browser -> {
-                    navController.navigate(R.id.fragmentBrowser, null, NavOptions.Builder()
-                        .setPopUpTo(R.id.fragmentBrowser, true)
-                        .build())
-                }
-                R.id.item_note -> {
-                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-                    supportActionBar?.displayOptions = ActionBar.DISPLAY_HOME_AS_UP
-                    navController.navigate(R.id.fragmentNote)
-                }
-                R.id.item_recycle_bin -> navController.navigate(R.id.fragmentRecycleBin)
-                R.id.item_setting -> navController.navigate(R.id.fragmentSetting)
-                R.id.item_language -> navController.navigate(R.id.fragmentLanguage)
-                R.id.item_theme -> changeTheme()
-                R.id.item_rate_app -> rateApp()
-                R.id.item_share_app -> shareApp()
-                R.id.item_about_us -> showAboutUs()
-                R.id.item_privacy -> showPrivacy()
-            }
-            true
-        }
+//        binding.navigationView.setNavigationItemSelectedListener {
+//            when (it.itemId) {
+////                R.id.fra -> navController.navigate(R.id.fragmentVault)
+////                R.id.item_browser -> {
+////                    navController.navigate(R.id.fragmentBrowser, null, NavOptions.Builder()
+////                        .setPopUpTo(R.id.fragmentBrowser, true)
+////                        .build())
+////                }
+////                R.id.item_note -> {
+////                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+////                    supportActionBar?.displayOptions = ActionBar.DISPLAY_HOME_AS_UP
+////                    navController.navigate(R.id.fragmentNote)
+////                }
+////                R.id.item_recycle_bin -> navController.navigate(R.id.fragmentRecycleBin)
+////                R.id.item_setting -> navController.navigate(R.id.fragmentSetting)
+////                R.id.item_language -> navController.navigate(R.id.fragmentLanguage)
+//                R.id.item_theme -> changeTheme()
+//                R.id.item_rate_app -> rateApp()
+//                R.id.item_share_app -> shareApp()
+//                R.id.item_about_us -> showAboutUs()
+//                R.id.item_privacy -> showPrivacy()
+//            }
+//            val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+//            drawerLayout.closeDrawer(GravityCompat.START)
+//            true
+//        }
     }
 
 
